e36d4e43bcf3788b6d43da01b46bce030ef47643
Robbie McCorkell <robbiemccorkell@gmail.com>
2019-11-19 21:55:39 +0000

    add home redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import { NextPage } from "next";
-import Layout from "../components/Layout";
+import Router from 'next/router'
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => (
-  <Layout>
-    <h1>Hello world! - user agent: {userAgent}</h1>
-  </Layout>
+const redirectUrl = "/blog";
+
+const Home: NextPage = () => (
+  <div />
 );
 
-Home.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
-  return { userAgent };
+Home.getInitialProps = async ({ res }) => {
+  if (res) {
+    res.writeHead(302, { Location: redirectUrl });
+    res.end();
+  } else {
+    Router.push(redirectUrl);
+  }
+
+  return {};
 };
 
 export default Home;
