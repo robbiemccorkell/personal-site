6ff339043b90561d48cb887994a82497879fd929
Robbie McCorkell <robbiemccorkell@gmail.com>
2019-11-25 18:30:01 +0000

    fix SlidesLive client side loading

--- components/SlidesLive.tsx
+++ components/SlidesLive.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect, useState } from "react";
+
 interface Props {
   presentationId: string;
   verticalEnabled?: boolean;
@@ -8,22 +10,44 @@ const SlidesLive: React.FunctionComponent<Props> = ({
   presentationId,
   verticalEnabled = true,
   autoPlay = false
-}) => (
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `
-    <div id="presentation-embed-${presentationId}"></div>
-    <script src='https://slideslive.com/embed_presentation.js'></script>
-    <script>
-        embed = new SlidesLiveEmbed('presentation-embed-${presentationId}', {
-            presentationId: '${presentationId}',
-            autoPlay: ${autoPlay},
-            verticalEnabled: ${verticalEnabled}
-        });
-    </script>
-  `
-    }}
-  />
-);
+}) => {
+  const script = `
+    embed = new SlidesLiveEmbed('presentation-embed-${presentationId}', {
+      presentationId: '${presentationId}',
+      autoPlay: ${autoPlay},
+      verticalEnabled: ${verticalEnabled}
+    });
+  `;
+
+  const elementRef = useRef<HTMLDivElement>(null);
+  const [mainScriptLoaded, updateScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      const mainScript = document.createElement("script");
+      mainScript.src = "https://slideslive.com/embed_presentation.js";
+      mainScript.async = true;
+      mainScript.onload = () => updateScriptLoaded(true);
+
+      elementRef.current.appendChild(mainScript);
+    }
+  }, [script]);
+
+  useEffect(() => {
+    if (mainScriptLoaded && elementRef.current) {
+      const embedScript = document.createElement("script");
+      embedScript.async = true;
+      embedScript.append(script);
+
+      elementRef.current.appendChild(embedScript);
+    }
+  }, [mainScriptLoaded]);
+
+  return (
+    <div ref={elementRef}>
+      <div id={`presentation-embed-${presentationId}`}></div>
+    </div>
+  );
+};
 
 export default SlidesLive;
