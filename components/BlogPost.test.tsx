ca1ffa2ea5ca163abaf3f31868599ab9932c8e81
Robbie McCorkell <robbiemccorkell@gmail.com>
2019-11-25 12:08:04 +0000

    blog post tests

---
+++ components/BlogPost.test.tsx
@@ -0,0 +1,44 @@
+import { render, getNodeText } from "@testing-library/react";
+import { RouterProvider, analytics } from "../test-utils";
+import BlogPost from "./BlogPost";
+
+test("renders children", () => {
+  const message = "Hello World";
+  const meta = {};
+  const router = {
+    pathname: "/blog/posts/some-post"
+  };
+  window.analytics = analytics;
+
+  const { queryByText } = render(
+    <RouterProvider value={router}>
+      <BlogPost meta={meta}>{message}</BlogPost>
+    </RouterProvider>
+  );
+
+  expect(queryByText(message)).not.toBeNull();
+});
+
+test("renders metadata", () => {
+  const message = "Hello World";
+  const meta = {
+    title: "My Blog",
+    canonical: "/other/blog",
+    date: "2019-01-01T12:00:00.000Z"
+  };
+  const router = {
+    pathname: "/blog/posts/some-post"
+  };
+  window.analytics = analytics;
+
+  const { container, queryByText } = render(
+    <RouterProvider value={router}>
+      <BlogPost meta={meta}>{message}</BlogPost>
+    </RouterProvider>
+  );
+
+  const h2 = getNodeText(container.querySelector("h2") || new HTMLElement());
+
+  expect(h2).toBe(meta.title);
+  expect(queryByText("1st January 2019", { exact: false })).not.toBeNull();
+});
---
+++ test-utils.tsx
@@ -0,0 +1,50 @@
+import { NextRouter } from "next/router";
+import { RouterContext } from "next/dist/next-server/lib/router-context";
+
+export const RouterProvider: React.FunctionComponent<{
+  value: Partial<NextRouter>;
+}> = ({ children, value }) => {
+  const router = {
+    pathname: "/blog/posts/some-post",
+    route: "/blog/posts/some-post",
+    query: {},
+    asPath: "/blog/posts/some-post",
+    push: async () => true,
+    replace: async () => true,
+    reload: () => null,
+    back: () => null,
+    prefetch: async () => undefined,
+    beforePopState: () => null,
+    events: {
+      on: () => null,
+      off: () => null,
+      emit: () => null
+    },
+    ...value
+  };
+
+  return (
+    <RouterContext.Provider value={router}>{children}</RouterContext.Provider>
+  );
+};
+
+export const analytics = {
+  use: jest.fn(),
+  init: jest.fn(),
+  addIntegration: jest.fn(),
+  setAnonymousId: jest.fn(),
+  load: jest.fn(),
+  identify: jest.fn(),
+  track: jest.fn(),
+  page: jest.fn(),
+  group: jest.fn(),
+  alias: jest.fn(),
+  trackLink: jest.fn(),
+  trackForm: jest.fn(),
+  ready: jest.fn(),
+  reset: jest.fn(),
+  user: jest.fn(),
+  debug: jest.fn(),
+  on: jest.fn(),
+  timeout: jest.fn()
+};
